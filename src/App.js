187378a44895bc10c1ae08403975729968c53a4b
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2021-12-22 22:02:19 +0100

    unsubscribe googleSignIn

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ import Homepage from './components/homepage/Homepage.component.js';
 import ShopPage from './components/shop/ShopPage.component';
 import Header from './components/header/Header.component';
 import SignInAndSignUp from './components/sign-in-and-sign-up-page/SignInAndSignUpPage.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div className='body'>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/sign" element={<SignInAndSignUp />}/>
-      </Routes>
 
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => { this.setState({ currentUser: user }); console.log(user) })
+
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className='body' >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/sign" element={<SignInAndSignUp />} />
+        </Routes>
+
+      </div>
+    );
+  }
 }
 
 export default App;
