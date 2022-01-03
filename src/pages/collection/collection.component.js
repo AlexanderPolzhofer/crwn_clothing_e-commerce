0d60edc0d2137d7e2b7006726d373afe19871dfc
AlexanderPolzhofer <Alexander.Polzhofer@gmail.com>
2022-01-03 12:21:19 +0100

    init collection-component

--- src/pages/collection/collection.component.js
+++ src/pages/collection/collection.component.js
@@ -1,25 +1,28 @@
 import React from 'react';
+
+import CollectionItem from '../../components/collection-item/CollectionItem.component';
+
 import { useParams } from 'react-router-dom';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import { useSelector } from 'react-redux';
+
 import './collection.styles.scss';
 
-
 const CollectionPage = () => {
-
 
     const { collectionId } = useParams();
     const collection = useSelector(selectCollection(collectionId));
 
-    console.log(collection)
-
     return (
         <div className='collection-page'>
-            <h1>{collectionId}</h1>
-            {collection.items.map(item => item.name)}
+            <h2 className='title'>{collectionId}</h2>
+            <div className='items'>
+                {collection.items.map(item =>
+                    <CollectionItem key={item.collectionId} item={item} />)}
+            </div>
+
         </div>
     );
 }
-
 
 export default CollectionPage;
