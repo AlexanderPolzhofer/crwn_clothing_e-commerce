2f77f68d31cd837f66d0d7bcd17ad1a05bd88cbe
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-03-05 18:05:36 +0100

    convert object from collections snapshot to new object

--- src/pages/shop/ShopPage.component.jsx
+++ src/pages/shop/ShopPage.component.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
 const ShopPage = () => {
+
+    //const unsubscribeFromSnapshot = null;
+
+    useEffect(() => {
+        const collectionRef = firestore.collection('collections');
+
+        collectionRef.onSnapshot(snapshot => {
+            convertCollectionsSnapshotToMap(snapshot);
+        })
+    }, []);
 
     let location = useLocation();
 
     const checkUrl = () => {
         if (location.pathname === '/shop') {
             return <CollectionsOverview />;
-        } else {
-            return <Outlet />;
         }
+        return <Outlet />;
     }
 
     return (< div className="shop-page" >
